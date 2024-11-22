f8f0819255f602b9240018bdd210261e51971ec3
Yoonstar <yagobo1110@naver.com>
2024-11-22 10:23:01 +0900

    ui 작업 전 준비 끝

--- next/board-renewal/app/layout.tsx
+++ next/board-renewal/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
+import { Toaster } from "@/components/ui/toast/toaster";
+// 스타일
 import "@/public/styles/globals.css";
+import "@/public/styles/main.scss";
+// 폰트
 import { FONT_NOTOSANSKR } from "@/public/assets/fonts";
 
 export const metadata: Metadata = {
@@ -13,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${FONT_NOTOSANSKR.className}`}>{children}</body>
+      <body className={`${FONT_NOTOSANSKR.className}`}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
