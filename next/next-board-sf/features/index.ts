c9da9541d5e73d483be6783d5c0963352d043791
Yoonstar <yagobo1110@naver.com>
2024-11-22 09:33:34 +0900

    기존 폴더 삭제

--- next/next-board-sf/features/index.ts
+++
@@ -1,5 +0,0 @@
-/** 공통 */
-export { AlertPopup } from "./common/popup/AlertPopup";
-/** 게시물 페이지 */
-export { CardBoard } from "./board/ui/CardBoard";
-export { MarkdownEditorDialog } from "./board/ui/ME-Dialog";
--- next/next-board-sf/hooks/supabase/index.ts
+++
@@ -1,1 +0,0 @@
-export { useTaskById } from "./read/use-task";
